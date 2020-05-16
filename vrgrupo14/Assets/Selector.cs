4aa68eb82c0120cce770dff6f298df95d9faded6
nesherben <nestor8299@gmail.com>
2020-05-16 20:03:40 +0200

    Sala wapisima y sight selector

---
+++ vrgrupo14/Assets/Selector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour
+{
+   
+    int contador = 3;
+    public void inp()
+    {
+        StartCoroutine("Countdown", contador);
+    }
+    public void outp() {
+        contador = 3;
+        StopCoroutine("Countdown");
+    }
+
+    private IEnumerator Countdown(int contador)
+    {
+        while (contador > 0)
+        {
+            Debug.Log(contador--);
+            yield return new WaitForSeconds(1);
+        }
+        Debug.Log("Countdown Complete!");
+    }
+}
