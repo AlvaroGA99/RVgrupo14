deddbd10ebd7f7b9b8281cb01fc9cc0aec4d4043
nesherben <nestor8299@gmail.com>
2020-05-20 20:15:08 +0200

    scene manager, sincronizacion de audios, prefabs y scripts

---
+++ vrgrupo14/Assets/Audios/prefabs/AudioTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTime : MonoBehaviour
+{
+    void Start()
+    {
+        this.gameObject.GetComponent<AudioSource>().enabled = false;
+    }
+    void Update()
+    {
+        if (Selector.tempo_control <= Mathf.Epsilon) {
+            Selector.flag_sound = false;
+
+            //como sincronizar las canciones en fase: mago.mago.mago(mago);
+            this.gameObject.GetComponent<AudioSource>().enabled = false;
+            this.gameObject.GetComponent<AudioSource>().enabled = true;
+            
+        }
+
+    }
+}
--- vrgrupo14/Assets/Selector.cs
+++ vrgrupo14/Assets/Selector.cs
@@ -7,6 +7,18 @@ public class Selector : MonoBehaviour
 {
     
     [SerializeField]int contador = 3;
+    public static double tempo = 9;
+    public static double tempo_control = tempo;
+    public static bool flag_exit = false;
+    public static bool flag_enter = false;
+    public static bool flag_sound;
+    Scene scene;
+
+    public void sincronizar()
+    {
+        tempo_control = 9;
+        flag_sound = true;
+    }
     public void inp()
     {
         StartCoroutine("Countdown", contador);
@@ -24,6 +36,40 @@ public class Selector : MonoBehaviour
             yield return new WaitForSeconds(1);
         }
         Debug.Log("Countdown Complete!");
-        
+        flag_exit = true; flag_enter = true;
     }
+
+    private void Start()
+    {
+        flag_exit = false; flag_enter = false;
+        scene = SceneManager.GetActiveScene();
+        sincronizar();
+    }
+    private void FixedUpdate()
+    {
+        if (flag_sound)
+        {
+            tempo_control -= Time.deltaTime;
+           // Debug.Log(tempo_control);
+        }
+        else sincronizar();
+
+        if (flag_exit && scene.name == "GameScene")
+        {
+            flag_exit = false;
+            SceneManager.LoadScene("StartMenu");
+
+        }
+        if (flag_enter && scene.name == "StartMenu")
+        {
+            flag_enter = false;
+            SceneManager.LoadScene("GameScene");
+
+        }
+
+
+    }
+
+
+
 }
